2321526ddd28f72523000057da3105b26403c2b1
Ludovick Bégin <ludovick.begin@gmail.com>
2018-11-29 14:28:13 -0500

    update constants. still not close

--- atomique/dev4num13.py
+++ atomique/dev4num13.py
@@ -12,12 +12,12 @@ for n in range(1, 3+1):
 
 nljNeutrons.append([1, 6, 6.5])
 nljProtons.append([1, 5, 5.5])
-print("Neutrons: ", nljNeutrons)
-print("Protons : ", nljProtons)
+# print("Neutrons: ", nljNeutrons)
+# print("Protons : ", nljProtons)
 
 A = 208
-V0 = 50
-hw = 41*A**(-1/3)
+V0 = 40
+hw = 54*A**(-1/3)
 D = 0.0225*hw
 C = 0.1*hw
 s = 1/2
@@ -27,11 +27,12 @@ def energy(index):
     n, l, j = index
     return (-V0 + hw*(2*(n-1) + l + 3/2) - D*l*(l+1) - (C/2)*(j*(j+1) - l*(l+1) - s*(s+1))) * (2*j + 1)
 
-
 energyNeutrons = [energy(index) for index in nljNeutrons]
 energyProtons = [energy(index) for index in nljProtons]
 
 totalEnergy = sum(energyNeutrons) + sum(energyProtons)
 
 print(sum(energyNeutrons), sum(energyProtons))
-print("Total Energy = ", totalEnergy)
+print("\nTotal Energy = {} MeV".format(round(totalEnergy, 2)))
+
+""" Valeur théorique: 1636 MeV """
