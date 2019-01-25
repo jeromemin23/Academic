69d0616d2d9c955cb33c5c9271657a7b1b82dd76
Arnaud Mercier <38730912+armercier@users.noreply.github.com>
2019-01-25 11:32:48 -0500

    small graph setup

---
+++ LaboInsru/graphAmpli.py
@@ -0,0 +1,36 @@
+import numpy as np
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib import pyplot as plt
+
+
+class Graph:
+
+    def __init__(self):
+        self.sauce = 1
+
+    def show(self):
+        filename = 'data.txt'
+        dataDict = self.importData(filename)
+        self.display(dataDict)
+
+    def importData(self, filename):
+        x, y = np.loadtxt('{}'.format(filename),unpack=True,delimiter=',')
+        dataDict = {'x':x,'y':y}
+        return dataDict
+
+    def display(self,dataDict):
+        plt.plot(dataDict['x'], dataDict['y'])
+        plt.show()
+
+
+    def normalizeABSPressure(self,dictData):
+         dataDictAbsNorm = []
+         maxABSP = max(dictData['absP'])
+         for absP in dictData['absP']:
+             dataDictAbsNorm.append(absP/maxABSP)
+         return dataDictAbsNorm
+
+
+d = Graph()
+d.show()
